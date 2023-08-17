327c4899670d58387fe5a47ed91555ca544cf70b
GabrielFrutuoso <mackbiel03@gmail.com>
2023-08-17 20:09:42 -0300

    testing

--- JS/script.js
+++ JS/script.js
@@ -8,33 +8,29 @@ const spots = document.querySelector('#spots')
 const danger = document.querySelector('#danger')
 const description = document.querySelector('#description')
 const image = document.querySelector('#image')
+const card  = document.querySelector('.card')
 
 buttons.forEach((button)=>{
     button.addEventListener('click', ()=>{
 
         const place = button.getAttribute("data-place")
 
-        changeTexts(
-            places[place].title,
-            places[place].habitants,
-            places[place].spots,
-            places[place].danger,
+        changeThings(
             places[place].description,
-            places[place].source
+            places[place].source,
+            places[place].color,
+            places[place].border,
         )
 
         modal.show()
     })
 })
 
-const changeTexts = (titulo, habitantes, lugares, perigo, descricao, imagem)=>{
+const changeThings = (descricao, titulo, cor, borda)=>{
     title.innerHTML = titulo
-    habitants.innerHTML = `habitantes: ${habitantes}`
-    spots.innerHTML = `Pontos de Interesse: ${lugares}`
-    danger.innerHTML = `Periculosidade: ${perigo}`
-    description.innerHTML = `descrição: ${descricao}`
-    image.src = `images/${imagem}.jpg`
-    
+    description.innerHTML = `Descrição: ${descricao}`
+    card.style.backgroundColor = cor
+    card.style.borderColor = borda
 }
 
 closeBtn.addEventListener('click', ()=>{
@@ -42,54 +38,48 @@ closeBtn.addEventListener('click', ()=>{
 })
 
 let places ={  
-        village: {
-            source: 'village',
-            title: 'Vila Muito Bacana',
-            habitants: 120,
-            spots: 'Mercador, Ferreiro, Igrejinha, Taverna',
-            danger: 'Baixa',
-            description: 'Uma pequena vila próxima do castelo, ela parece ser uma vila muito bacana.'
+        covilGeno: {
+            source: 'Covil Geno',
+            description: 'Principal Fortaleza e refúgio do clã geno nesse reino, n cheira mt bem por fora pode provocar nauseas',
+            color: '#A65D03',
+            border: '#BF7E04'
         },
-        mill: {
-            source: 'mill',
-            title: 'Cata-vento',
-            habitants: 1,
-            spots: 'cata-vento',
-            danger: 'Baixa',
-            description: 'Uma cata-vento onde vive velho fazendeiro.'
+        vilaAna: {
+            source: 'vila Anã',
+            description: 'Uma vila comum de anões onde pode de se encontrar muitas cavernas e minas para minerar, todos são muito amigaveis',
+            color: '#3E2723',
+            border: '#5D4037'
         },
-        lightHouse: {
-            source: 'lighthouse',
-            title: 'Velho Farol',
-            habitants: 0,
-            spots: 'o velho farol',
-            danger: 'Baixa',
-            description: 'Um velho farol do antigo ponto, aquele lugar me dá arrepios!'
-        },
-        Forest: {
-            source: 'forest',
-            title: 'Floresta Apavorante',
-            habitants: '?',
-            spots: 'Vila do Duendes da floresta',
-            danger: 'moderada',
-            description: 'Uma densa floresta repleta de criaturas, onde há boatos de uma vila de duendes'
+        forest: {
+            source: 'Floresta Morta',
+            description: 'Há lendas que um dia essa floresta ja teve vida e agia por si msm, tendo como consciência o espirito da floresta porém desde as últimas eras a floresta perdeu sua essência e suas criaturas',
+            color: '#18261E',
+            border: '#365942'
         },
         castle: {
-            source: 'castle',
-            title: 'Castelo Real',
-            habitants: 587,
-            spots: 'Loja de armaduras, Taverna do Rei',
-            danger: 'baixa',
-            description: 'Uma densa floresta repleta de criaturas, onde há boatos de uma vila de duendes'
+            source: 'Castelo',
+            description: 'Em algum tempo ja foi um lugar de honra e destaque, agora é um simbolo de medo e temor',
+            color: '#56067F',
+            border: '#693E7F'
         },
-        farm: {
-            source: 'farm',
-            title: 'Fazenda Real',
-            habitants: 24,
-            spots: 'Estábulo, Plantações',
-            danger: 'baixa',
-            description: 'A fazenda Real, onde é nala platada diversas leguminosas e vegetais, além de conter diversos animais, todos os seus recursos vão para o Castelo Ceal'
+        village: {
+            source: 'Vila',
+            description: 'A vila em volta do castelo existe antes mesmo da construção do próprio, a mão de obra para o lider é cruelmente retirada de lá',
+            color: '#56067F',
+            border: '#693E7F'
         },
+        pike: {
+            source: 'Pico',
+            description: 'Há poucas casas, inclusive a de um muito conhecido inventor',
+            color: '#585859',
+            border: '#BABBBF'
+        },
+        cave: {
+            source: 'Cavernas',
+            description: 'Ninguém nunca saiu de lá o mesmo, muitos dizem haver monstros terríveis ali ou uma magia bizarra capaz de mudar a aparência e memórias',
+            color: '#585859',
+            border: '#BABBBF'
+        },
+        
     }
-
 
